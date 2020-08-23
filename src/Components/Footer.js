d23bd1bf40b7c7436b73f456d27340ddf257dd8c
Abdullah Al Mamun <mhmamun166009@gmail.com>
2020-08-23 22:49:22 +0600

    okk

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './styles/footer.scss';
 import { Link } from 'react-router-dom';
-// import { Icon } from 'react-icons-kit';
-// import {socialFacebook, socialYoutube, socialTwitter} from 'react-icons-kit/typicons';
+import { Icon } from 'react-icons-kit';
+import { socialFacebook, socialYoutube, socialTwitter } from 'react-icons-kit/typicons';
+import { envelope } from 'react-icons-kit/fa/envelope';
 
 import itVillageLogo from '../assets/static/it-village-logo.png';
 
@@ -12,7 +13,7 @@ const Footer = () => {
         <div className="footer">
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-lg-7 pr-lg-5 text-center text-lg-left">
+                    <div className="col-12 col-lg-6 pr-lg-5 text-center text-lg-left">
                         <h4 className="mb-4">Our Partners</h4>
                         <div className="card border-0">
                             <div className="card-body partners">
@@ -28,8 +29,33 @@ const Footer = () => {
                         <Link to="/">terms</Link>
                         <Link to="/">helpline</Link>
                     </div>
-                    <div className="col-12 col-lg-2 mt-5 mt-lg-0 text-center text-lg-left">
-                        <h4>Contacts</h4>
+                    <div className="col-12 col-lg-3 mt-5 mt-lg-0 text-center text-lg-left">
+                        <h4 className="mb-4">Contacts</h4>
+                        <div className="social">
+                            <ul>
+                                <li>
+                                    <a href="https://www.facebook.com/elearners.live">
+                                        <Icon icon={socialFacebook} size={25} />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.youtube.com/elearners.live">
+                                        <Icon icon={socialYoutube} size={25} />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/">
+                                        <Icon icon={socialTwitter} size={25} />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="mailto:webmaster@example.com">
+                                        <Icon icon={envelope} size={25} />
+                                    </a>
+                                </li>
+                            </ul>
+
+                        </div>
                     </div>
                 </div>
             </div>
