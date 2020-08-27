5627b6ad0ed99250ad3c5c3308d26b3b52739023
Abdullah Al Mamun <mhmamun166009@gmail.com>
2020-08-27 08:07:53 +0600

    okk

--- src/Pages/ClassRoom/CourseShow.js
+++ src/Pages/ClassRoom/CourseShow.js
@@ -4,7 +4,7 @@ import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import PageLoading from '../../Components/Loading';
 
-import CourseImg from '../../assets/courses/coding.png';
+import CourseImg from '../../assets/courses/Bangla_Goddo.png';
 
 const CourseShow = () => {
     const [courses, setCourses] = useState([])
@@ -15,10 +15,10 @@ const CourseShow = () => {
     useEffect(() => {
         const fetchUser = () => {
             setLoading(true)
-            axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${section}`)
+            axios.get(`https://jsonplaceholder.typicode.com/posts`)
                 .then(res => {
 
-                    setCourses(res.data);
+                    setCourses(res.data.slice(0,15));
                     setLoading(false)
                 })
         }
@@ -35,19 +35,24 @@ const CourseShow = () => {
                         <h4 className="mb-0 ml-2">ওয়েব ডিজাইন</h4>
                     </div>
 
-                    {courses.length > 0 && courses.map((course, i) =>
-                        <div className="card course-card" key={i}>
-                            <div className="card-body shadow-sm">
-                                <img src={CourseImg} className="img-fluid" alt="..." />
-                                <p>ওয়েব ডিজাইন</p>
-                                <Link
-                                    to={`/classroom/course/${course.id}`}
-                                    type="button"
-                                    className="btn btn-sm shadow-none"
-                                >এখানে ক্লিক করুন</Link>
-                            </div>
+                    <div className="card shadow-sm">
+                        <div className="card-body">
+                            {courses.length > 0 && courses.map((course, i) =>
+
+                                <Link to={`/classroom/course/${course.id}`} key={i}>
+                                    <div className="course-box">
+                                        <img src={CourseImg} className="img-fluid" alt="..." />
+                                        <p className="text-muted">ওয়েব ডিজাইন</p>
+                                        {/* <Link to={`/classroom/course/${course.id}`}
+                                        type="button"
+                                        className="btn btn-sm shadow-none"
+                                    >এখানে ক্লিক করুন</Link> */}
+                                    </div>
+                                </Link>
+
+                            )}
                         </div>
-                    )}
+                    </div>
 
                 </div>
             }
